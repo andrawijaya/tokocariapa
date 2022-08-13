3c77b489bcf8bb60b5c9e16cd719e697cf917842
Andra <andrawijaya170798@gmail.com>
2022-08-13 09:22:05 +0700

    upload first project

---
+++ frontend/src/components/Home/Home.js
@@ -0,0 +1,52 @@
+import React, { Fragment, useEffect } from "react";
+import { CgMouse } from "react-icons/cg";
+import "./Home.css";
+import Product from "./ProductCard.js";
+import MetaData from "../layout/MetaData";
+import { clearErrors, getProduct } from "../../actions/productAction";
+import { useDispatch, useSelector } from "react-redux";
+import Loader from "../layout/Loader/Loader";
+import { useAlert } from "react-alert";
+
+const Home = () => {
+  const alert = useAlert();
+  const dispatch = useDispatch();
+  const { loading, error, products } = useSelector((state) => state.products);
+
+  useEffect(() => {
+    if (error) {
+      alert.error(error);
+      dispatch(clearErrors());
+    }
+    dispatch(getProduct());
+  }, [dispatch, alert, error]);
+
+  return (
+    <Fragment>
+      {loading ? (
+        <Loader />
+      ) : (
+        <Fragment>
+          <MetaData title="Toko Cariapa" />
+          <div className="banner">
+            <p>Selamat Datang di TokoCariapa</p>
+            <h1>Temukan Produk Luar Biasa di bawah ini</h1>
+
+            <a href="#container">
+              <button>
+                Scroll <CgMouse />{" "}
+              </button>
+            </a>
+          </div>
+          <h2 className="homeHeading">Produk Unggulan</h2>
+          <div className="container" id="container">
+            {products &&
+              products.map((product) => <Product product={product} />)}
+          </div>
+        </Fragment>
+      )}
+    </Fragment>
+  );
+};
+
+export default Home;
